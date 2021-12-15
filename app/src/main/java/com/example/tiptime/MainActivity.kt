76c88ba40e8c217fa331c1cebac236365f9bae8a
LakeishaMcCree <lmccree215@icloud.com>
2021-12-15 11:04:25 -0500

    Fixed calculating errors using debugger

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -6,7 +6,7 @@ import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+   lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,27 +15,28 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener { calculateTip() }
     }
 
+
     //helper method
-    private fun calculateTip() {
+    fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
             if (cost == null) {
                 binding.tipResult.text = ""
                 return
             }
-
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
         var tip = tipPercentage * cost
-            if (binding.roundUpSwitch.isChecked) {
-                tip = kotlin.math.ceil(tip)
-        }
-        val formattedTip = NumberFormat.getCurrencyInstance().format(0.0)
+        val roundUp = binding.roundUpSwitch.isChecked
+            if (roundUp) {
+            kotlin.math.ceil(tip)
+            }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
 
-        NumberFormat.getCurrencyInstance()
+        //NumberFormat.getCurrencyInstance()
     }
 }
